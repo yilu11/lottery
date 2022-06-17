9cc26b8e3889356adee1b41828d494262172a7eb
yilu11 <yilu11@qq.com>
2022-06-17 17:27:39 +0800

    Updates

---
+++ static/js/31.15a0c7c3.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/widgets"]=this["webpackJsonp@uniswap/widgets"]||[]).push([[31],{732:function(s,p){s.exports={messages:{}}}}]);
+//# sourceMappingURL=31.15a0c7c3.chunk.js.map
---
+++ static/js/8.c2df5086.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/widgets"]=this["webpackJsonp@uniswap/widgets"]||[]).push([[8],{1085:function(n,i){},1094:function(n,i){},1096:function(n,i){}}]);
+//# sourceMappingURL=8.c2df5086.chunk.js.map
